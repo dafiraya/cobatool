965b6583bfd29bf7b1b910ded75835b0f8f2a021
dafir <81427133+dafiraya@users.noreply.github.com>
2021-04-06 20:07:55 +0700

    coba edit tool 1

---
+++ cobatol.sh
@@ -0,0 +1,39 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay  Dafira
+echo $green "Oktafiani"
+echo
+sleep 2
+echo $yellow"pilih programku"
+echo
+echo $cyan"1.)Stabilkan ya"
+echo 
+echo $blue"0.)keluar ya"
+echo
+echo $white
+read -p "Pilih ya : " mas
+if [$mas = 1 ] | [$mas = 1 ]
+then
+clear
+toilet -f big -F gay Dafira
+echo $green "Stabilkan ya Dafira"
+sleep 2
+ping  1.1.1.1
+fi
+if [$mas = 0 ] |[$mas = 0 ] 
+then
+clear
+echo $yellow
+figlet "Bye"
+echo $white"To Be Continued"
+sleep 3
+exit
+fi
